06c2e030093b6c0f4b458a60228ff3f05eb7f5b2
iceman2146 <iceman2146@mail.ru>
2024-03-16 16:29:32 +0300

    kk

--- cpp/3_StandLib/2/class_work_count_word.cpp
+++ cpp/3_StandLib/2/class_work_count_word.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45095419ec25448142959fe3df3299d7d22b1fa122fde7fd5820f16308b856e1
-size 1916
+oid sha256:36dde4c8888ecb6a2a400b3dad510544481094a04e99d9f7fed0b6f28ec50fcd
+size 4530

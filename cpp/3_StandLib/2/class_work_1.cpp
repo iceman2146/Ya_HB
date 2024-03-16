930bd20cf4feab543748d926964369600219569a
iceman2146 <iceman2146@mail.ru>
2024-03-16 15:59:15 +0300

    words_counter

--- cpp/3_StandLib/2/class_work_1.cpp
+++ cpp/3_StandLib/2/class_work_1.cpp
@@ -1,31 +1,3 @@
-#include <iostream>
-#include <map>
-#include <string>
-int main()
-{
-	std::map<std::string, int> data;
-	std::string key;
-	int value;
-	while (std::cin >> key >> value)
-	{
-		data[key] = value;  // вставка
-	}
-	data.erase("hello"); 
-	 // удаление
-	for (const auto& [key, value] : data) 
-	{
-	std::cout << key << ": " << value << "\n";
-	}
-	 // поиск
-	 if (auto iter = data.find("test");iter != data.end())
-	{
-		std::cout << "Found the key " 
-					<< iter->first 
-					<< " with the value " 
-					<< iter->second << "\n";
-    } 
-    else
-    {
-	    std::cout << "Not found\n";
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:801934a0d4a89c4d21f43450b3d12d5ef439a89d9ac1d0c4a2f20d67fd6d0f32
+size 1627
---
+++ cpp/3_StandLib/2/class_work_count_word.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:45095419ec25448142959fe3df3299d7d22b1fa122fde7fd5820f16308b856e1
+size 1916
